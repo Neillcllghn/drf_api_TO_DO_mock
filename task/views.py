e22a2678882e2b3471eba1b76e954fb1509204b3
Neillcllghn <>
2023-07-15 08:18:47 +0000

    create post form for tasks and permissions

--- task/views.py
+++ task/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Task
@@ -6,9 +6,25 @@ from .serializers import TaskSerializer
 
 
 class TaskList(APIView):
+    serializer_class = TaskSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
     def get(self, request):
         tasks = Task.objects.all()
-        serializers = TaskSerializer(
+        serializer = TaskSerializer(
             tasks, many=True, context={'request': request}
             )
-        return Response(serializers.data)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TaskSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
