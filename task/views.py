509551e22428685ebc1d2ffacce40665089e394e
Neillcllghn <>
2023-09-19 18:59:34 +0000

    Updated response and status

--- task/views.py
+++ task/views.py
@@ -77,6 +77,8 @@ from .serializers import TaskSerializer, TaskDetailSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class TaskList(generics.ListCreateAPIView):
